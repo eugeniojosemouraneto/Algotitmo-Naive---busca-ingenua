104109a338202d01cec9bbd126503a6875eebe5d
Eugênio José Moura Neto <158313815+eugeniojosemouraneto@users.noreply.github.com>
2024-11-09 22:20:10 +0000

    modificação basicas nas funções já feitas

--- main.cpp
+++ main.cpp
@@ -1,44 +1,46 @@
+#include "naiveAlgorithm.cpp"
+
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-string analyzedText = "morangoorangorangorcngo";
+string analyzedText = "morangoorangorangoracngo";
 string analyzedStandard = "rango";
 
-int textSize(string& text) {
-    int count = 0;
-    for(count; text[count] != '\0'; count++) {}
-    return --count;
-}
+// int textSize(string& text) {
+//     int count = 0;
+//     for(count; text[count] != '\0'; count++) {}
+//     return --count;
+// }
 
-vector<pair<int, int>> naiveMethod(string& text, string& standard) {
-    vector <pair<int, int>> positions;  
-    for(int i = 0; text[i] != '\0'; i++) {
-        if(text[i] == standard[0]) {
-            int j=0;
-            while(standard[j] != '\0' and text[i+j+1] == standard[j+1]) j++;
-            if(text[i+j] == standard[textSize(standard)]) positions.push_back({i, i+j});
-        }
-    }
-    return positions;
-}
+// vector<pair<int, int>> naiveMethod(string& text, string& standard) {
+//     vector <pair<int, int>> positions;  
+//     for(int i = 0; text[i] != '\0'; i++) {
+//         if(text[i] == standard[0]) {
+//             int j=0;
+//             while(standard[j] != '\0' and text[i+j] == standard[j]) j++;
+//             if(text[i+j] == standard[textSize(standard)]) positions.push_back({i, i+j-1});
+//         }
+//     }
+//     return positions;
+// }
 
-void run() {
-    vector <pair<int,int>> positions;
-    positions = naiveMethod(analyzedText, analyzedStandard);
-    int numberAppearancesPattern = positions.size();
-    cout << "Number of appearances of the pattern " << numberAppearancesPattern << '\n';
-    for(int i = 0; i < numberAppearancesPattern; i++) {
-        int start = positions[i].first, end = positions[i].second;
-        cout << '[' << start << '-' << end <<"] - ";
-        for(start; start <= end; start++) {
-            cout << analyzedText[start];
-        }
-        cout << '\n';
-    }
-}
+// void run() {
+//     vector <pair<int,int>> positions;
+//     positions = naiveMethod(analyzedText, analyzedStandard);
+//     int numberAppearancesPattern = positions.size();
+//     cout << "Number of appearances of the pattern " << numberAppearancesPattern << '\n';
+//     for(int i = 0; i < numberAppearancesPattern; i++) {
+//         int start = positions[i].first, end = positions[i].second;
+//         cout << '[' << start << '-' << end <<"] - ";
+//         for(start; start <= end; start++) {
+//             cout << analyzedText[start];
+//         }
+//         cout << '\n';
+//     }
+// }
 
 int main() {
-    run();
+    NaiveAlgorithm naive(analyzedText, analyzedStandard);
+    vector<pair<int,int>> aux=naive.runAlgorithm().first;
 }
--- naiveAlgorithm.cpp
+++ naiveAlgorithm.cpp
@@ -1,25 +1,26 @@
 #include <vector>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 class NaiveAlgorithm {
+    int standardSize;
     string text, standard;
     vector <pair<int, int>> positionsAppearancesPattern;
 
-
-    int textSize(string& text) {
+    int textSize(string& t) {
         int count = 0;
-        for(count; this -> text[count] != '\0'; count++) {}
-        return --count;
+        for(count; t[count] != '\0'; count++) {}
+        return count;
     }
     
     vector <pair<int,int>> NaiveMethod() {
         for(int i = 0; this -> text[i] != '\0'; i++) {
             if(this -> text[i] == standard[0]) {
                 int j = 0;
-                while(this -> standard[j] != '\0' and this -> text[i+j+1] == this -> standard[j+1]) j++;
-                if(this -> text[i+j] == this -> standard[textSize(standard)]) this -> positionsAppearancesPattern.push_back({i, i+j});
+                while(this -> standard[j] != '\0' and this -> text[i+j] == this -> standard[j]) {j++;}
+                if(this -> text[i+j-1] == this -> standard[this -> standardSize -1]) {this -> positionsAppearancesPattern.push_back({i, i+j-1});}
             }
         }
         return this -> positionsAppearancesPattern;
@@ -30,6 +31,8 @@ class NaiveAlgorithm {
         NaiveAlgorithm(string& text, string& standard) {
             this -> text     = text;
             this -> standard = standard;
+
+            this -> standardSize = textSize(standard);
         }
 
         void  clearPatternPositions() {
@@ -48,11 +51,12 @@ class NaiveAlgorithm {
             return this -> positionsAppearancesPattern.size();
         }
 
-        pair<int, vector<pair<int, int>>> runAlgorithm() {
+        pair<vector<pair<int, int>>, int> runAlgorithm() {
             this -> clearPatternPositions();
+
             return {
-                this -> GetNumberPatternPositions(), 
-                this -> NaiveMethod()
+                this -> NaiveMethod(),
+                this -> GetNumberPatternPositions() 
             };
         }
 };
